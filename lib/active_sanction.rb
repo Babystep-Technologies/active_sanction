5cbb6c19c87b821a3bce692d76156ddd219ac265
Marshall Shen <shen.marshall@gmail.com>
2026-08-28 19:30:20 -0500

    initial commit

---
+++ lib/active_sanction.rb
@@ -0,0 +1,6 @@
+require "active_sanction/version"
+
+module ActiveSanction
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_sanction/version.rb
@@ -0,0 +1,3 @@
+module ActiveSanction
+  VERSION = "0.1.0"
+end
